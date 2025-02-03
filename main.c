b9dc42bd371e6530c79adb5ff17614cde4c09b4f
Paul Cadman <git@paulcadman.dev>
2025-02-03 09:36:39 +0000

    Add example fragment shader

--- main.c
+++ main.c
@@ -1,9 +1,29 @@
 #include <raylib.h>
 
 int main(void) {
-    InitWindow(800, 600, "Shader");
+    const int screenWidth = 500;
+    const int screenHeight = 500;
+    InitWindow(screenWidth, screenHeight, "Shader");
+    Shader shader = LoadShader(0, "main.fs");
+
+    int resolutionLoc = GetShaderLocation(shader, "resolution");
+
+    float resolution[2] = {(float) screenWidth, (float) screenHeight};
+    SetShaderValue(shader, resolutionLoc, resolution, SHADER_UNIFORM_VEC2);
+
+    // Load blank texture to draw on using shader
+    Image imBlank = GenImageColor(screenWidth, screenHeight, BLANK);
+    Texture2D texture = LoadTextureFromImage(imBlank);
+    UnloadImage(imBlank);
+
+    SetTargetFPS(60);
     while (!WindowShouldClose()) {
         BeginDrawing();
+        ClearBackground(BLACK);
+        BeginShaderMode(shader);
+        DrawTexture(texture, 0, 0, BLANK);
+        EndShaderMode();
+        DrawFPS(700, 15);
         EndDrawing();
     }
     CloseWindow();
---
+++ main.fs
@@ -0,0 +1,19 @@
+#version 330
+
+in vec2 fragTexCoord;
+in vec4 fragColor;
+
+uniform vec2 resolution;
+
+out vec4 finalColor;
+
+void main() {
+    vec2 uv = fragTexCoord.xy;
+
+    uv = vec2(uv.x, 1.0 - uv.y);
+    uv = uv - 0.5;
+
+    float d = length(uv);
+    // finalColor = vec4(uv.x, uv.y, 0.0, 1.0);
+    finalColor = vec4(d, d, 0.0, 1.0);
+}
